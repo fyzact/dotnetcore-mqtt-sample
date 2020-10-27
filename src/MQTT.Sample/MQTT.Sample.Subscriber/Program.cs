5b7a72bc56060e7a0860be5e613d630a3d2b375a
fyzact <feyyazacet@gmail.com>
2020-10-27 16:16:56 +0300

    create subcriber project

---
+++ src/MQTT.Sample/MQTT.Sample.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTT.Sample.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
