f0abc9ec25c3526084116adf111c86ff29035f4a
fyzact <feyyazacet@gmail.com>
2020-10-27 16:02:18 +0300

    implement m2mMqttlibrary

--- src/MQTT.Sample/MQTT.Sample.Publisher/Program.cs
+++ src/MQTT.Sample/MQTT.Sample.Publisher/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MQTT.Sample.Publisher
 {
@@ -6,7 +9,19 @@ namespace MQTT.Sample.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create client istance
+            MqttClient client = new MqttClient("localhost", 1883, false, MqttSslProtocols.None, null, null);
+            string clientId = Guid.NewGuid().ToString();
+            //connected
+            client.Connect(clientId, "mqtt-test", "mqtt-test");
+
+            while (true)
+            {
+                //publis message
+                var message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message)) break;
+                client.Publish("/home/test1", Encoding.UTF8.GetBytes(message));
+            }
         }
     }
 }
