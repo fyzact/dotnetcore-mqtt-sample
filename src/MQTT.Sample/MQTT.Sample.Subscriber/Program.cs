4011403fc1cb33699dd7fc627f30583d8a087781
fyzact <feyyazacet@gmail.com>
2020-10-27 16:21:42 +0300

    implement m2mmqttdotnetcore library to subscribe

--- src/MQTT.Sample/MQTT.Sample.Subscriber/Program.cs
+++ src/MQTT.Sample/MQTT.Sample.Subscriber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace MQTT.Sample.Subscriber
 {
@@ -6,7 +8,21 @@ namespace MQTT.Sample.Subscriber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MqttClient client = new MqttClient("localhost", 1883, false, MqttSslProtocols.None, null, null);
+
+            string clientId = Guid.NewGuid().ToString();
+            byte f = client.Connect(clientId, "mqtt-test", "mqtt-test");
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // subscribe to the topic
+            client.Subscribe(new string[] { "/home/test1" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            Console.ReadKey();
+        }
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = System.Text.Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine(message);
         }
     }
 }
